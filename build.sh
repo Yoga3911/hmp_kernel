f5b71ee015543238ad92ef58eee5cbc2562717d9
Robin Hood <74900723+Yoga3911@users.noreply.github.com>
2021-05-09 05:21:20 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export KERNELNAME=Excalibur
+
+export LOCALVERSION=
+
+export KBUILD_BUILD_USER=ExcaliburZx
+
+export KBUILD_BUILD_HOST=FunProject
+
+export TOOLCHAIN=clang
+
+export DEVICES=lavender
+
+source helper
+
+gen_toolchain
+
+send_msg "⏳ Mulai memasak ${KERNELNAME} ${LOCALVERSION} for ${DEVICES}... ⏳"
+
+START=$(date +"%s")
+
+for i in ${DEVICES//,/ }
+do 
+
+	build ${i} -oldcam
+
+done
+
+END=$(date +"%s")
+ 
+DIFF=$(( END - START ))
